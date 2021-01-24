59645f89e1a680fdbf718d7767c5d65d9a3134d4
MORISHITA Makoto <maktm0525@gmail.com>
2021-01-24 15:55:40 +0900

    recog2

--- test_extractaudio.py
+++ test_extractaudio.py
@@ -1,10 +1,11 @@
 import subprocess
 
 inputFile = "IosaWkNnRYw.mp4"
-#outputFile = "IosaWkNnRYw.flac"
-outputFile = "IosaWkNnRYw.wav"
+outputFile = "IosaWkNnRYw.flac"
+# outputFile = "IosaWkNnRYw.wav"
 
 ## flac
 #cmd = "ffmpeg -i {0} -vn -ar 44100 -ac 2 -acodec flac -f flac {1}".format(inputFile, outputFile)
-cmd = "ffmpeg -i {0} -f wav {1}".format(inputFile, outputFile)
+cmd = "ffmpeg -i {0} -vn -ar 44100 -ac 1 -acodec flac -f flac {1}".format(inputFile, outputFile)
+# cmd = "ffmpeg -i {0} -f wav {1}".format(inputFile, outputFile)
 subprocess.call(cmd, shell=True)
